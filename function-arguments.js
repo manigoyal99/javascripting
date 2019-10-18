c4bd724c0b890cd1f3559264457b4fef03aba6ce
Mani Goyal <mani73842@gmail.com>
2019-10-18 14:02:25 +0530

    javascript

---
+++ function-arguments.js
@@ -0,0 +1,5 @@
+function math(one,two,three){
+    var a=one+(two*three);
+    return a;
+}
+console.log(math(53,61,67));
---
+++ revising-strings.js
@@ -0,0 +1,3 @@
+let pizza="pizza is alright";
+var i=pizza.replace("alright","wonderful");
+console.log(i);
